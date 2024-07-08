8ec28e1404ae3a2cabe52ce8e8b4a7d92ccf52fb
jaesungchris <work.jaesung.n@gmail.com>
2024-07-08 23:28:07 +0900

    feat: mantine-vanilla-extract config

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,8 @@
+import '@mantine/core/styles.css'
+import { MantineProvider } from '@mantine/core'
+import { theme } from './theme'
 const App = () => {
-  return <div>hello-world</div>
+  return <MantineProvider theme={theme}>hello-world</MantineProvider>
 }
 
 export default App
