237db9448e8f53a4eeaa875dd509b7878259ac2c
jaesungchris <112853438+jaesungchris@users.noreply.github.com>
2024-07-22 23:42:39 +0900

    feat: add SectionBody component (#20)

--- packages/client/src/contract/pages/PageContractEdit.tsx
+++ packages/client/src/contract/pages/PageContractEdit.tsx
@@ -1,14 +1,17 @@
 import { useParams } from 'react-router-dom'
 import FormContract from '../components/FormContract.tsx'
 import { ContractEditPathParamsType } from '../path.config.ts'
-import SectionPage from '../../_app/components/section-page/SectionPage.tsx'
+import SectionPage from '../../_app/components/section/SectionPage.tsx'
+import SectionBody from '../../_app/components/section/SectionBody.tsx'
 
 const PageContractEdit = () => {
   const params = useParams<ContractEditPathParamsType>()
   console.log(params.contractId)
   return (
     <SectionPage>
-      <FormContract />
+      <SectionBody>
+        <FormContract />
+      </SectionBody>
     </SectionPage>
   )
 }
--- packages/client/src/customer/pages/PageCustomerEdit.tsx
+++ packages/client/src/customer/pages/PageCustomerEdit.tsx
@@ -1,14 +1,17 @@
 import { useParams } from 'react-router-dom'
 import FormCustomer from '../components/FormCustomer.tsx'
 import type { CustomerEditPathParamsType } from '../path.config.ts'
-import SectionPage from '../../_app/components/section-page/SectionPage.tsx'
+import SectionPage from '../../_app/components/section/SectionPage.tsx'
+import SectionBody from '../../_app/components/section/SectionBody.tsx'
 
 const PageCustomerEdit = () => {
   const params = useParams<CustomerEditPathParamsType>()
   console.log(params.customerId)
   return (
     <SectionPage>
-      <FormCustomer />
+      <SectionBody>
+        <FormCustomer />
+      </SectionBody>
     </SectionPage>
   )
 }
